32565b7763361bcaa7cbc111f59801b20146e66c
Isabela Fabian <isafbn@gmail.com>
2024-04-09 09:19:21 -0300

    904

--- routes/rotasCategorias.js
+++ routes/rotasCategorias.js
@@ -1,15 +1,18 @@
 const { Router } = require('express');
 
+const {verificaJWT} = require('../controllers/segurancaController');
+
+
 const {getCategorias, addCategorias, updateCategorias, deleteCategorias, getCategoriaPorCodigo} = require ('../controllers/categoriaController')
 
 const rotasCategorias = new Router();
 
 rotasCategorias.route('/categoria')
-    .get(getCategorias)
-    .post (addCategorias)
-    .put (updateCategorias)
+    .get(verificaJWT,getCategorias)
+    .post (verificaJWT,addCategorias)
+    .put (verificaJWT,updateCategorias)
 rotasCategorias.route('/categoria/:codigo')
-    .get(getCategoriaPorCodigo)
-    .delete (deleteCategorias)
+    .get(verificaJWT, getCategoriaPorCodigo)
+    .delete (verificaJWT, deleteCategorias)
 
 module.exports = {rotasCategorias};
